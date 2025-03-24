16926e2882a7f4e70f70b136abc6d3081f18d973
Rafi076 <mohiuddinrafi076.com>
2025-03-25 00:50:18 +0600

    Splash Screen Ui design

--- simple_e_commerce_app/lib/app.dart
+++ simple_e_commerce_app/lib/app.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:simple_e_commerce_app/presentaion/ui/splash.dart';
+import 'package:simple_e_commerce_app/presentaion/ui/utils/app_colors.dart';
 
 class CraftBayApp extends StatelessWidget {
   const CraftBayApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return  MaterialApp(
+      home: SplashScreen(),
+      theme: ThemeData(
+        colorSchemeSeed: AppColors.themeColor,
+
+        scaffoldBackgroundColor: Colors.white,
+
+        progressIndicatorTheme: const ProgressIndicatorThemeData(
+          color: AppColors.themeColor,
+        )
+
+      ),
+    );
   }
 }
--- simple_e_commerce_app/test/widget_test.dart
+++ simple_e_commerce_app/test/widget_test.dart
@@ -14,7 +14,7 @@ import 'package:simple_e_commerce_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(CraftBayApp());
+    await tester.pumpWidget(const CraftBayApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
