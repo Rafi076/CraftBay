a6afac98e4943623f3c47674f6bdca831d93a974
Rafi076 <mohiuddinrafi076.com>
2025-04-03 03:43:40 +0600

    Product List Screen

---
+++ simple_e_commerce_app/lib/presentaion/ui/screens/product_list_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../widgets/popular_card.dart';
+
+class ProductListScreen extends StatelessWidget {
+  const ProductListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        titleTextStyle: const TextStyle(
+          color: Colors.black54,
+          fontSize: 20,
+          fontWeight: FontWeight.w500,
+        ),
+        title: const Text('Electronics'),// Style icons
+      ),
+      body: GridView.builder(
+          itemCount: 20,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,childAspectRatio: 0.75, mainAxisSpacing: 5, crossAxisSpacing: 10),
+          itemBuilder: (context, index) {
+            return const popularCard();
+          }),
+    );
+  }
+}
--- simple_e_commerce_app/lib/presentaion/ui/widgets/category_card.dart
+++ simple_e_commerce_app/lib/presentaion/ui/widgets/category_card.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:simple_e_commerce_app/presentaion/ui/screens/product_list_screen.dart';
 
 import '../utils/app_colors.dart';
 
@@ -10,27 +13,32 @@ class CategoryCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          padding: const EdgeInsets.all(26),
-          decoration: BoxDecoration(
-              color: AppColors.themeColor.withOpacity(0.1),
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(8),
-                topRight: Radius.circular(8),
-              )),
-          child: const Icon(Icons.computer,
-              size: 48, color: AppColors.themeColor),
-        ),
-        const SizedBox(
-          height: 4,
-        ),
-        const Text(
-          'Electronics',
-          style: TextStyle(color: AppColors.themeColor),
-        )
-      ],
+    return GestureDetector(
+      onTap: (){
+        Get.to(()=> ProductListScreen());
+      },
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(26),
+            decoration: BoxDecoration(
+                color: AppColors.themeColor.withOpacity(0.1),
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(8),
+                  topRight: Radius.circular(8),
+                )),
+            child: const Icon(Icons.computer,
+                size: 48, color: AppColors.themeColor),
+          ),
+          const SizedBox(
+            height: 4,
+          ),
+          const Text(
+            'Electronics',
+            style: TextStyle(color: AppColors.themeColor),
+          )
+        ],
+      ),
     );
   }
 }
